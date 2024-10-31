14337e8ed62f7982f3b2a08f0e53eaedbdb637a9
Natalya Malenko <nataljuna@gmail.com>
2024-10-31 17:56:03 +0200

    done tasks

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+function getElementWidth(content, padding, border) {
+  content = parseFloat(content);
+  padding = parseFloat(padding);
+  border = parseFloat(border);
+  console.log(content + padding * 2 + border * 2);
+}
+
+getElementWidth("50px", "8px", "4px");
+getElementWidth("60px", "12px", "8.5px");
+getElementWidth("200px", "0px", "0px");
