3c3f803a561e0bf1d4ef5c67e886d8e7a91e1727
Natalya Malenko <nataljuna@gmail.com>
2024-11-01 12:45:47 +0200

    fix bugs

--- js/task-3.js
+++ js/task-3.js
@@ -2,9 +2,14 @@ function getElementWidth(content, padding, border) {
   content = parseFloat(content);
   padding = parseFloat(padding);
   border = parseFloat(border);
-  console.log(content + padding * 2 + border * 2);
+  const totalWidth = content + padding * 2 + border * 2;
+  return totalWidth;
 }
 
 getElementWidth("50px", "8px", "4px");
 getElementWidth("60px", "12px", "8.5px");
 getElementWidth("200px", "0px", "0px");
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
